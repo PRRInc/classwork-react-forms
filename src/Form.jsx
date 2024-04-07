da01b1a89656c7676124333e220f10cfeebdfe76
PRRInc <prrincdev@gmail.com>
2024-04-07 17:30:52 -0400

    Classwork React Forms Complete

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,12 +1,53 @@
 import React from "react";
+import { useState } from "react";
 import "./Form.css";
 
 function Form() {
+
+  const [nums, setNums] = useState("");
+  const [selectOperation, setSelectOperation] = useState("");
+  const [result, setResult] = useState(0);
+
+  function updateInput(event) {
+    setNums(event.target.value);
+  }
+
+  function handleSelectChange(event) {
+    setSelectOperation(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (selectOperation === "sum") {
+      setResult(nums.split(",").reduce((acc,currVal) => acc + Number(currVal), 0));
+    } else if (selectOperation === "average") {
+      setResult(nums.split(",").reduce((acc,currVal) => acc + Number(currVal), 0)/nums.split(",").length);
+    } else if (selectOperation === "mode") {
+      const resultObj = {};
+      let most = 0;
+      let finalKey = "";
+      let numsArray = nums.split(",");
+      for(let num in numsArray) {
+        resultObj[numsArray[num]] = (resultObj[numsArray[num]] || 0) + 1;
+      }
+      for(let numVal in resultObj) {
+        const value = resultObj[numVal]
+        if (value > most) {
+          most = value;
+          finalKey = numVal;
+        }
+      }
+      setResult(finalKey)
+  }
+}
+
+
+  
   return (
     <>
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
+      <form onSubmit={handleSubmit}>
+        <input id="values" name="values" type="text" value={nums} onChange={updateInput}/>
+        <select id="operation" name="operation" onChange={handleSelectChange}>
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
@@ -15,7 +56,7 @@ function Form() {
         <button type="submit">Calculate</button>
       </form>
       <section id="result">
-        <p></p>
+        <p>{result}</p>
       </section>
     </>
   );
